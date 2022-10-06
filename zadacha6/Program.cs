92191343d4f2670ddda1e4c1e7a59c9d5b649c77
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-06 22:25:10 +0600

    решил задачу 6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

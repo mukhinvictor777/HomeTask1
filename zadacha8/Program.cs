49c7c26a11e41864a6ee9fa81e67af671cb1ebfb
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-06 22:48:26 +0600

    Решил задачу 8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int countnumber = 1;
+if (Number < 2)
+{
+   Console.WriteLine($"Четных положительных числел в выбранном диапазоне от 1 до {Number} нет");
+}
+else 
+{
+    while (countnumber <= Number)
+    {
+        if (countnumber % 2 == 0)
+        {
+            Console.WriteLine($"{countnumber}");
+            countnumber++;
+        }
+        else
+        {
+            countnumber++;
+        }
+    }
+}

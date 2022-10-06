6846b05a5eae8cfca531ba76d296b4cb90e19516
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-06 22:00:46 +0600

    Решил задачу 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+if (userNumberA == userNumberB)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (userNumberA > userNumberB)
+    {
+    Console.WriteLine($"max = {userNumberA}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {userNumberB}");
+    }
+}

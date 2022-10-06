fa12c4c78ea877cae0379cabd9fd74cbd0ba3647
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-06 22:16:29 +0600

    решил задачу 4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int userNumberC = Convert.ToInt32(Console.ReadLine());
+int max = userNumberA;
+if (userNumberA < userNumberB)
+{
+    max = userNumberB;
+    if (userNumberB<userNumberC)
+    {
+        max = userNumberC;
+    }
+}
+else
+{
+    if (userNumberA < userNumberC)
+    {
+    max = userNumberC;
+    }
+}
+Console.WriteLine($"max = {max}");
